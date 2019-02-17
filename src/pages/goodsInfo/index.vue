fdaea3757b7aa210a16476989be70e07fe456452
yw910915 <602142325@qq.com>
2019-02-17 15:13:13 +0800

    goodsinfo完成

--- src/pages/goodsInfo/index.vue
+++ src/pages/goodsInfo/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="goodsinfo-container">
-    <div class="ball" v-show="ballFlag"></div>
+    <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" name="ball"> 
+      <!-- 添加ref属性可以操作dom元素 -->
+      <div class="ball" v-show="ballFlag" ref="ball"></div> 
+    </transition>
     <!-- 商品轮播图区域 -->
     <div class="mui-card">
       <div class="mui-card-content">
@@ -75,7 +78,7 @@ export default {
             goodsinfo:{},//获取到的商品信息
             buyCount:1,
             ballFlag: false, // 控制小球的隐藏和显示的标识符
-
+            selectedCount:1 // 保存用户选中的商品数量， 默认，认为用户买1个
         }
     },
     created() {
@@ -120,9 +123,43 @@ export default {
           // this.$router.push({ path: "/home/goodscomment/" + id
           this.$router.push("/home/goodsComment/"+id)
         },
-        addToShopCar(){
+        addToShopCar(){ //小球添加到购物车
           this.ballFlag=!this.ballFlag
+        },
+        //开始执行动画之前执行的钩子函数
+        beforeEnter(el){
+          el.style.transform="translate(0,0)"
+        },
+        // 开始执行动画后的钩子函数
+        enter(el,done){
+          // 就是得加   offsetHeight
+          el.offsetWidth;
+          // 小球动画优化思路：
+          // 1. 先分析导致 动画 不准确的 本质原因： 我们把 小球 最终 位移到的 位置，已经局限在了某一分辨率下的 滚动条未滚动的情况下；
+          // 2. 只要分辨率和 测试的时候不一样，或者 滚动条有一定的滚动距离之后， 问题就出现了；
+          // 3. 因此，我们经过分析，得到结论： 不能把 位置的 横纵坐标 直接写死了，而是应该 根据不同情况，动态计算这个坐标值；
+          // 4. 经过分析，得出解题思路： 先得到 徽标的 横纵 坐标，再得到 小球的 横纵坐标，然后 让 y 值 求差， x 值也求 差，得到 的结果，就是横纵坐标要位移的距离
+          // 5. 如何 获取 徽标和小球的 位置？？？   domObject.getBoundingClientRect()
+
+          // 获取小球的 在页面中的位置
+          const ballPosition=this.$refs.ball.getBoundingClientRect()
+          // 获取 徽标 在页面中的位置
+          const badgePosition=document.getElementById('badge').getBoundingClientRect()
+          const xDist=badgePosition.left-ballPosition.left
+          const yDist=badgePosition.top-ballPosition.top
+
+          el.style.transform=`translate(${xDist}px,${yDist}px)`;
+          el.style.transition = 'all 0.5s cubic-bezier(.4,-0.3,1,.68)';
+          done();
+        },
+        // 动画执行完毕后的钩子函数
+        afterEnter(el){
+          this.ballFlag=!this.ballFlag
+        },
+        getSelectedCount(count){
+          this.selectedCount=count
         }
+
     },
 }
 </script>
